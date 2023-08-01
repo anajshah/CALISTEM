e8118fe1345df2595fc352f7a03661837acd2f32
Aariana <anajshah@gmail.com>
2023-08-01 14:03:39 -0700

    Initial Commit

---
+++ CALISTEM/CALISTEMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CALISTEMApp.swift
+//  CALISTEM
+//
+//  Created by Aariana Shah on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CALISTEMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
